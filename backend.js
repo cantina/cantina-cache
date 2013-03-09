76f3641192c90210921f068023a529734c653f76
Brian Link <cpsubrian@gmail.com>
2013-03-08 17:45:37 -0800

    Fixes some bugs/race-conditions.

--- backend.js
+++ backend.js
@@ -29,9 +29,9 @@ CantinaBackend.prototype.key = function () {
 
 CantinaBackend.prototype.set = function (options, cb) {
   var self = this;
-  this.backends.redis.set(options, function (err) {
+  self.backends.memory.set(options, function (err) {
     if (err) return cb(err);
-    self.backends.memory.set(options, function (err) {
+    self.backends.redis.set(options, function (err) {
       if (err) return cb(err);
       self.amino.publish(self.key('clear:memory'), options.key, self.amino.id);
       cb();
@@ -41,18 +41,24 @@ CantinaBackend.prototype.set = function (options, cb) {
 
 CantinaBackend.prototype.get = function (key, cb) {
   var self = this;
-  this.backends.memory.get(key, function (err, result) {
+  self.backends.memory.get(key, function (err, result) {
     if (err) return cb(err);
     if (result) return cb(null, result);
-    self.backends.redis.get(key, cb);
+    self.backends.redis.get(key, function (err, result) {
+      if (err) return cb(err);
+      if (!result) return cb(null, null);
+      self.backends.memory.set(result, function (err) {
+        cb(err, result);
+      });
+    });
   });
 };
 
 CantinaBackend.prototype.invalidate = function (tags, cb) {
   var self = this;
-  this.backends.redis.invalidate(tags, function (err) {
+  self.backends.memory.invalidate(tags, function (err) {
     if (err) return cb(err);
-    self.backends.memory.invalidate(tags, function (err) {
+    self.backends.redis.invalidate(tags, function (err) {
       if (err) return cb(err);
       self.amino.publish(self.key('invalidate:memory'), tags, self.amino.id);
       cb();
@@ -62,9 +68,9 @@ CantinaBackend.prototype.invalidate = function (tags, cb) {
 
 CantinaBackend.prototype.clear = function (pattern, cb) {
   var self = this;
-  this.backends.redis.clear(pattern, function (err) {
+  self.backends.memory.clear(pattern, function (err) {
     if (err) return cb(err);
-    self.backends.memory.clear(pattern, function (err) {
+    self.backends.redis.clear(pattern, function (err) {
       if (err) return cb(err);
       self.amino.publish(self.key('clear:memory'), pattern, self.amino.id);
       cb();
